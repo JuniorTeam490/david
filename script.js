2790723a03e22e14f9002c01e9ab3add86758675
JuniorTeam490 <atomgepard@gmail.com>
2025-06-17 18:25:36 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const buttons = document.querySelectorAll('.accordion-button');
-
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        const content = btn.nextElementSibling;
-        content.style.display = content.style.display === 'block' ? 'none' : 'block';
-    });
-});
